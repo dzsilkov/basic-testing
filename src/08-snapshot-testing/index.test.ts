9d04459678788d5a3f09482cf6dcf9c6ba852d11
dzianis <dzsilkov@gmail.com>
2024-02-11 22:29:15 +0100

    implemented partial mocking, mocking node api, mocking lib api and snapshot testing tests

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,32 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
+
+const elements = [1, 2, 3, 4 ];
+const expectedResult = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: {
+          value: null,
+          next: null
+        }
+      }
+    }
+  }
+};
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    expect(generateLinkedList(elements)).toStrictEqual(expectedResult);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    expect(generateLinkedList(elements)).toMatchSnapshot();
   });
 });
